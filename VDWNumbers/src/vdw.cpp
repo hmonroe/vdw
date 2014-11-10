93b4ccf3692b4790af29cf2175a6dac944e835b2
Hunter <Hunter@MaterroLLC.home>
2014-11-09 20:02:36 -0500

    Verbose mode

--- VDWNumbers/src/vdw.cpp
+++ VDWNumbers/src/vdw.cpp
@@ -1,7 +1,6 @@
 // Find van der Waerden lower bounds by constructing certificates using 
 // the power residue method.
 
-//#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 #include <fstream>
@@ -9,10 +8,11 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
-
+bool verbose = false;
 
 void writeBestPrimes(long long candidateprime, long long bestprimes[100][100]) {
-	cout << "Saving:" << candidateprime << endl;
+	if (verbose)
+		cout << "Saving:" << candidateprime << endl;
 	ofstream outputfile;
 	outputfile.open("vdwtable.csv");
 	if (outputfile.is_open())
@@ -22,12 +22,10 @@ void writeBestPrimes(long long candidateprime, long long bestprimes[100][100]) {
 		for (int color = 2; color < 10; color++) {
 			if (outputfile.is_open()) {
 				outputfile << bestprimes[color][lengths] << ",";
-//				cout << bestprimes[color][lengths] << ",";
 			}
 		}
 		if (outputfile.is_open()) {
 			outputfile << endl;
-//			cout << endl;
 		}
 	}
 	outputfile.close();
@@ -36,14 +34,15 @@ void writeBestPrimes(long long candidateprime, long long bestprimes[100][100]) {
 long long readBestPrimes(long long bestprimes[100][100]) {
 	ifstream inputfile;
 	inputfile.open("vdwtable.csv");
-	long long startrange = 2;
+	long long startrange = 3;
 	if (inputfile.is_open()) {
 		if (inputfile) {
 			string s;
 			if (getline(inputfile, s)) {
 				istringstream ss(s);
 				startrange = atoi(s.c_str());
-				cout << "Read start range: " << startrange << endl;
+				if (verbose)
+					cout << "Read start range: " << startrange << endl;
 			}
 		}
 		int lengths = 3;
@@ -66,9 +65,9 @@ long long readBestPrimes(long long bestprimes[100][100]) {
 		}
 		inputfile.close();
 	} else {
-		cout << "Error opening file" << endl;
+		if (verbose)
+			cout << "Error opening file" << endl;
 	}
-//	cout << "Finished reading input " << endl;
 	return startrange;
 }
 
@@ -86,7 +85,6 @@ bool isPrime(long long candidateprime) {
 
 long long writeEveryThousand(long long candidateprime, long long threshold,
 		long long bestprimes[100][100]) {
-	//	  cout<<"Prime "<<prime<<endl;
 	if (candidateprime > threshold) {
 		writeBestPrimes(candidateprime, bestprimes);
 		threshold = threshold + 1000;
@@ -95,6 +93,7 @@ long long writeEveryThousand(long long candidateprime, long long threshold,
 }
 
 int main() {
+
 	long long maxnumcolors = 6;
 	long long bestprimes[100][100];
 	for (int i = 0; i < 100; i++)
@@ -104,22 +103,24 @@ int main() {
 	long long startrange = readBestPrimes(bestprimes);
 	// look for primes and use them to color a list
 	long long threshold = startrange + 1000;
-	for (long long candidateprime = startrange;; candidateprime++) {
+	for (long long candidateprime = startrange;;
+			candidateprime = candidateprime + 2) {
 		if (isPrime(candidateprime)) {
-//	  cout<<"Prime "<<prime<<endl;
+			if (verbose)
+				cout << "Prime: " << candidateprime << endl;
 			threshold = writeEveryThousand(candidateprime, threshold,
 					bestprimes);
-
 			//use prime to color the list
 			long long* colors = NULL;
 			colors = new long long[(unsigned int) candidateprime - 1 + 20];
-//	  long long* zipcolors = NULL;
-//	  zipcolors=new long long[(unsigned int)prime-1+20];
 			for (long long i = 1; i < candidateprime; i++)
 				colors[i] = 0;
 			//find a primitive root of the prime
 			for (long long candidateroot = 2;
 					candidateroot < candidateprime - 1; candidateroot++) {
+				if (verbose)
+					cout << "Candidate root mod " << candidateprime << ": "
+							<< candidateroot << endl;
 				colors[candidateroot] = 1;
 				bool isroot = true;
 				long long power = candidateroot;
@@ -127,33 +128,26 @@ int main() {
 				for (long long exponent = 2; exponent <= candidateprime - 1;
 						exponent++) {
 					power = (power * candidateroot) % candidateprime;
-					if (power == 1 && (exponent < candidateprime - 1))
+					if (verbose)
+						cout << candidateroot << "^" << exponent << "=" << power
+								<< ";";
+					if (power == 1 && (exponent < candidateprime - 1)) {
 						isroot = false;
+						if (verbose)
+							cout << endl << "Not a primitive root mod "
+									<< candidateprime << ": " << candidateroot
+									<< endl;
+						break;
+					}
 					colors[power] = exponent;
 				}
-//		zipcolors[0]=0;
-//		for(long long position=1; position<=prime-1; position++) {
-//		  if ((colors[position] % 4)==0) zipcolors[position]=0;
-//		  if ((colors[position] % 4)==1) zipcolors[position]=1;
-//		  if ((colors[position] % 4)==2) zipcolors[position]=2;
-//		  if ((colors[position] % 4)==3) zipcolors[position]=3;
-//		  if ((colors[position] % 4)==4) zipcolors[position]=4;
-//		  if ((colors[position] % 4)==5) zipcolors[position]=5;
-//		}
-//
-
-//		for(long long position=0; position<=prime-1; position++) {
-//		  cout<<zipcolors[position]; //<<",";
-//		}
-				//with 2 colors there is no need to find a primitive root
-				//for(long long exponent=2; exponent<=prime/2; exponent++) {
-				//power=exponent * exponent % prime;
-				//if (power==1 ) colors[power] = 1;
-				//}
-
 				if (isroot) {
-//		  cout<<"root: "<<candidateroot<<endl;
+					if (verbose)
+						cout << endl << "Primitive root found mod "
+								<< candidateprime << ": " << candidateroot
+								<< endl;
 					// check conditions
+					//break;
 					long long* lastentry = NULL;
 					lastentry = new long long[(unsigned int) maxnumcolors + 1];
 					long long* longestsequence = NULL;
@@ -214,10 +208,32 @@ int main() {
 			}
 			delete[] colors;
 			colors = NULL;
-//	  delete [] zipcolors;
-//	  zipcolors=NULL;
 		}
 	}
 	return 0;
 }
 
+//	  long long* zipcolors = NULL;
+//	  zipcolors=new long long[(unsigned int)prime-1+20];
+//		zipcolors[0]=0;
+//		for(long long position=1; position<=prime-1; position++) {
+//		  if ((colors[position] % 4)==0) zipcolors[position]=0;
+//		  if ((colors[position] % 4)==1) zipcolors[position]=1;
+//		  if ((colors[position] % 4)==2) zipcolors[position]=2;
+//		  if ((colors[position] % 4)==3) zipcolors[position]=3;
+//		  if ((colors[position] % 4)==4) zipcolors[position]=4;
+//		  if ((colors[position] % 4)==5) zipcolors[position]=5;
+//		}
+//
+
+//		for(long long position=0; position<=prime-1; position++) {
+//		  if(verbose) cout<<zipcolors[position]; //<<",";
+//		}
+//with 2 colors there is no need to find a primitive root
+//for(long long exponent=2; exponent<=prime/2; exponent++) {
+//power=exponent * exponent % prime;
+//if (power==1 ) colors[power] = 1;
+//}
+
+//	  delete [] zipcolors;
+//	  zipcolors=NULL;
