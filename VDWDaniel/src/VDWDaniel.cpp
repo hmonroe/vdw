34624fa985b2d3ddd404b63812a2698878285043
Daniel <daniel-monroe@verizon.net>
2015-01-25 20:00:00 -0500

    Added comments that tell what the computer did

--- VDWDaniel/src/VDWDaniel.cpp
+++ VDWDaniel/src/VDWDaniel.cpp
@@ -7,10 +7,14 @@
 using namespace std;
 
 int main() {
+	// creates array of best primes of each and initializes it to zero
 	int bestPrimes[100];
 	for (int i = 0; i < 100; i++)
 		bestPrimes[i] = 0;
+
 	long startValue = 3;
+
+	//opens vdwtable.csv and reads the file into bestPrimes
 	ifstream inputfile;
 	inputfile.open("vdwtable.csv");
 	int lengths = 0;
@@ -36,14 +40,19 @@ int main() {
 		}
 		inputfile.close();
 	}
+
 	for (long possiblePrime = startValue; possiblePrime < startValue + 99;
 			possiblePrime++) {
 		bool isPrime = true;
+		//checks if possiblePrime is prime
 		for (long possibleFactor = 2; possibleFactor < sqrt(possiblePrime) + 1;
 				possibleFactor++)
-			if (possiblePrime % possibleFactor == 0)
+			if (possiblePrime % possibleFactor == 0) {
 				isPrime = false;
+				break;
+			}
 		if (isPrime) {
+			//checks if possibleRoot is a primitive root
 			for (long possibleRoot = 2; possibleRoot < possiblePrime;
 					possibleRoot++) {
 				int powers[possiblePrime];
@@ -65,9 +74,11 @@ int main() {
 
 				}
 				cout << endl;
+				//If possibleRoot is a primitve root, then check
 				if (isPrimitive) {
 //					cout << possibleRoot << " is a primitive root of "
 //							<< possiblePrime << "." << endl;
+					//check what the longest sequence for this prime is
 					long currentLength = 1;
 					long finalLength = 1;
 					for (long position = 2; position <= possiblePrime - 1;
@@ -83,8 +94,11 @@ int main() {
 					}
 					cout << endl;
 
+
 					cout << "Length " << finalLength + 1 << " " << possiblePrime
 							<< endl;
+					// If possiblePrime is greater then the previous bestPrime for that length,
+					// set the bestPrimes to the possiblePrime
 					if (possiblePrime > bestPrimes[finalLength]) {
 						bestPrimes[finalLength] = possiblePrime;
 //						cout <<"better prime"<< bestPrimes[finalLength] << endl;
@@ -96,11 +110,12 @@ int main() {
 		}
 	}
 
+	// Writes the best primes into daniel.csv
 	ofstream outputfile;
 	outputfile.open("daniel.csv");
 	if (outputfile.is_open()) {
 		for (int i = 1; i < 24; i++)
-			outputfile << bestPrimes[i] << " , " << i + 1 << endl;
+			outputfile << bestPrimes[i] << endl;
 		outputfile << endl;
 		outputfile << ",";
 	}
